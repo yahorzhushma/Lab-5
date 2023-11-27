b829b23b69b7ee0d5a3f9c5edf3c79d902e00004
yahorzhushma <yahorzhushma@gmail.com>
2023-11-27 20:49:56 +0300

    3.2

--- 3.2.py
+++ 3.2.py
@@ -1,6 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+def set_labels():
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
 x = np.linspace(-10, 10, 500)
 y = np.linspace(-10, 10, 500)
 
@@ -9,49 +14,39 @@ x, y = np.meshgrid(x, y)
 z1 = x**0.25 + y**0.25
 surf = plt.figure()
 ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z1, cmap='viridis')
+ax.plot_surface(x, y, z1)
 plt.title('График функции z1')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+set_labels()
 plt.show()
 
 z2 = x**2 - y**2
 surf = plt.figure()
 ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z2, cmap='viridis')
+ax.plot_surface(x, y, z2)
 plt.title('График функции z2')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+set_labels()
 plt.show()
 
 z3 = 2*x + 3*y
 surf = plt.figure()
 ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z3, cmap='viridis')
+ax.plot_surface(x, y, z3)
 plt.title('График функции z3')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+set_labels()
 plt.show()
 
 z4 = x**2 + y**2
 surf = plt.figure()
 ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z4, cmap='viridis')
+ax.plot_surface(x, y, z4)
 plt.title('График функции z4')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+set_labels()
 plt.show()
 
 z5 = 2 + 2*x + 2*y - x**2 - y**2
 surf = plt.figure()
 ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z5, cmap='viridis')
+ax.plot_surface(x, y, z5)
 plt.title('График функции z5')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+set_labels()
 plt.show()
