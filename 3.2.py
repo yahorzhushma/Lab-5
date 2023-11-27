2c1792412db4b9cdefcc40f66eca48df19f708c8
yahorzhushma <yahorzhushma@gmail.com>
2023-11-28 00:30:56 +0300

    3.2

--- 3.2.py
+++ 3.2.py
@@ -1,10 +1,17 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def set_labels():
+
+def create_surface(z, title):
+    surf = plt.figure()
+    ax = surf.add_subplot(111, projection='3d')
+    ax.plot_surface(x, y, z)
+    plt.title(title)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
+    plt.show()
+
 
 x = np.linspace(-10, 10, 500)
 y = np.linspace(-10, 10, 500)
@@ -12,41 +19,16 @@ y = np.linspace(-10, 10, 500)
 x, y = np.meshgrid(x, y)
 
 z1 = x**0.25 + y**0.25
-surf = plt.figure()
-ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z1)
-plt.title('График функции z1')
-set_labels()
-plt.show()
+create_surface(z1, 'График функции z1')
 
 z2 = x**2 - y**2
-surf = plt.figure()
-ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z2)
-plt.title('График функции z2')
-set_labels()
-plt.show()
+create_surface(z2, 'График функции z2')
 
 z3 = 2*x + 3*y
-surf = plt.figure()
-ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z3)
-plt.title('График функции z3')
-set_labels()
-plt.show()
+create_surface(z3, 'График функции z3')
 
 z4 = x**2 + y**2
-surf = plt.figure()
-ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z4)
-plt.title('График функции z4')
-set_labels()
-plt.show()
+create_surface(z4, 'График функции z4')
 
 z5 = 2 + 2*x + 2*y - x**2 - y**2
-surf = plt.figure()
-ax = surf.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z5)
-plt.title('График функции z5')
-set_labels()
-plt.show()
+create_surface(z5, 'График функции z5')
